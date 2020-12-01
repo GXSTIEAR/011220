35dde8cd4b5a4cd82fe62f0bcc771bd055bf8663
GXSTIEAR <74530763+GXSTIEAR@users.noreply.github.com>
2020-12-01 12:44:16 +0500

    01.12.2020

---
+++ directory#1/step_1.py
@@ -0,0 +1,11 @@
+# PEP - 8
+# snake case
+my_var = 5 #сущность класса инт
+print (my_var, type(my_var), dir(my_var))*
+
+my_var = "5" #сущность класса инт
+print (my_var, type(my_var), dir(my_var))
+
+my_var = 5.0 #сущность класса инт
+print (my_var, type(my_var), dir(my_var))
+
---
+++ directory#1/step_2.py
@@ -0,0 +1,7 @@
+my_war = "5"
+print(my_var, type(my_var))
+print(dir(my_var))
+
+txt = "Клубника бомба честно говоря, сам ёё люблю"
+print(txt.upper())
+print(txt.lower())
